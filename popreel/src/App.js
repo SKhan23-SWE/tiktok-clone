c59c6fb18b772d8bc96ebf0a0472c0ecb2609fe4
SohaibK-23 <Khansohaib98@yahoo.com>
2025-02-05 19:03:56 -0600

    Starter work
    Starting stage of my PopReels full-stack app

---
+++ popreel/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      // BEM naming convention
+      <h1>Building a PopReels app</h1>
+    </div>
+  );
+}
+
+export default App;
